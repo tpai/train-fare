608b69c05c858af2764a66ea4d9b9a7d065a3168
Tony Pai <tony77794@gmail.com>
2020-06-06 21:28:55 +0800

    Refactor UI

--- template/index.js
+++ template/index.js
@@ -1,10 +1,9 @@
 const stations = [{"value":"BC_","label":"Bologna"},{"value":"BLZ","label":"Bolzano"},{"value":"BSC","label":"Brescia"},{"value":"DSG","label":"Desenzano"},{"value":"F__","label":"Ferrara"},{"value":"SMN","label":"Firenze SMN"},{"value":"MI0","label":"Milano (Tutte)"},{"value":"MC_","label":"Milano Centrale"},{"value":"RRO","label":"Milano Rho Fiera"},{"value":"RG_","label":"Milano Rog"},{"value":"NAF","label":"Napoli Afragola"},{"value":"NAC","label":"Napoli C.le"},{"value":"PD_","label":"Padova"},{"value":"PSY","label":"Peschiera"},{"value":"AAV","label":"Reggio Emilia AV"},{"value":"RM0","label":"Roma (Tutte)"},{"value":"RMT","label":"Roma Termini"},{"value":"RTB","label":"Roma Tib"},{"value":"RVR","label":"Rovereto"},{"value":"R__","label":"Rovigo"},{"value":"SAL","label":"Salerno"},{"value":"TOP","label":"Torino P.Nuova"},{"value":"OUE","label":"Torino P.Susa"},{"value":"TCN","label":"Trento"},{"value":"VE0","label":"Venezia (Tutte)"},{"value":"VEM","label":"Venezia Mestre"},{"value":"VSL","label":"Venezia S.Lucia"},{"value":"VPN","label":"Verona P.N."},{"value":"VIC","label":"Vicenza"}];
-const classes = [{label:"All",value:"O"},{label:"Smart",value:"S"},{label:"Prima",value:"P"},{label:"Comfort",value:"T"},{label:"Club",value:"C"}];
-const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null,belongsTo:"P"},{label:"All",value:null,belongsTo:"T"},{label:"All",value:null,belongsTo:"C"},{label:"All",value:null,belongsTo:"O"},{label:"Promo",value:"H",belongsTo:"S"},{label:"Low Cost",value:"U",belongsTo:"S"},{label:"Economy",value:"T",belongsTo:"S"},{label:"Flex",value:"N",belongsTo:"S"},{label:"Low Cost",value:"B1",belongsTo:"P"},{label:"Economy",value:"M1",belongsTo:"P"},{label:"Flex",value:"G",belongsTo:"P"},{label:"Low Cost",value:"S",belongsTo:"T"},{label:"Economy",value:"F",belongsTo:"T"},{label:"Flex",value:"Z",belongsTo:"T"},{label:"Economy",value:"X",belongsTo:"C"},{label:"Flex",value:"A",belongsTo:"C"}];
+const classes = [{label:"All",value:"O"},{label:"Smart",value:"S"},{label:"Prima",value:"P"},{label:"Comfort",value:"T"},{label:"Club",value:"C"},{label:"Salotto1",value:"S1"},{label:"Salotto2",value:"S2"}];
+const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null,belongsTo:"P"},{label:"All",value:null,belongsTo:"T"},{label:"All",value:null,belongsTo:"C"},{label:"All",value:null,belongsTo:"O"},{label:"Promo",value:"H",belongsTo:"S"},{label:"Low Cost",value:"U",belongsTo:"S"},{label:"Economy",value:"T",belongsTo:"S"},{label:"Flex",value:"N",belongsTo:"S"},{label:"Low Cost",value:"B1",belongsTo:"P"},{label:"Economy",value:"M1",belongsTo:"P"},{label:"Flex",value:"G",belongsTo:"P"},{label:"Low Cost",value:"S",belongsTo:"T"},{label:"Economy",value:"F",belongsTo:"T"},{label:"Flex",value:"Z",belongsTo:"T"},{label:"Economy",value:"X",belongsTo:"C"},{label:"Flex",value:"A",belongsTo:"C"},{label:"Flex",value:"W0",belongsTo:"S1"},{label:"Flex",value:"Y0",belongsTo:"S2"}];
 
 (function() {
   let backup;
-  let sig;
 
   const isRound = document.querySelector('#is_round');
   const returnDate = document.querySelector('#return_date');
@@ -17,10 +16,14 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
     }
   });
 
-  const depart = document.querySelector('#depart');
-  const arrival = document.querySelector('#arrival');
+  const departDateInput = document.querySelector('input[name="interval_start"]');
+  const returnDateInput = document.querySelector('input[name="round_start"]');
 
-  window.onload = () => depart.focus();
+  window.onload = () => {
+    const today = moment(new Date()).format('YYYY-MM-DD');
+    departDateInput.value = today;
+    returnDateInput.value = today;
+  };
 
   const classSelector = document.querySelector('#class');
   const serviceSelector = document.querySelector('#service');
@@ -34,18 +37,11 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
   const submit = document.querySelector('#submit');
   const share = document.querySelector('#share');
   const result = document.querySelector('#result');
-  const username = document.querySelector('#username');
-  const password = document.querySelector('#password');
 
   new ClipboardJS(share);
   share.addEventListener('click', () => alert('Link copied!'));
 
   const startQuery = () => {
-    if (username.value === '' || password === '') {
-      alert('You must provide Italo username and password for searching.\n\nNotice: Your account information is only use for querying available train, please do not use if have any concern.');
-      return;
-    }
-
     const {
       depart,
       arrival,
@@ -93,6 +89,7 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
     };
 
     result.style.display = 'block';
+    result.style.minHeight = '100px';
     result.innerHTML = `
     <div class="ts active inverted dimmer">
       <div class="ts text loader">Loading...</div>
@@ -101,29 +98,10 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
 
     async.waterfall([
       function (callback) {
-        if (sig) {
-          callback(null, sig);
-          return;
-        }
-        fetch('/api/login', {
-          method: 'POST',
-          headers: { 'content-type': 'application/json' },
-          body: JSON.stringify({
-            username: username.value,
-            password: password.value,
-          }),
-        })
-          .then(res => res.json())
-          .then(({ Signature }) => {
-            sig = Signature;
-            callback(null, Signature);
-          });
-      },
-      function (sig, callback) {
         fetch('/api/getAvailableTrains', {
           method: 'POST',
           headers: { 'content-type': 'application/json' },
-          body: JSON.stringify({ sig, ...body }),
+          body: JSON.stringify(body),
         })
           .then(res => res.json())
           .then(json => callback(null, json));
@@ -132,7 +110,13 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
       if (JourneyDateMarkets && JourneyDateMarkets.length > 0 && JourneyDateMarkets[0].Journeys !== null) {
         const journeys = JourneyDateMarkets[0].Journeys.filter(({ Segments }) => Segments[0].Fares !== null);
         const filteredJourneys = priceFilter(journeys);
-        result.innerHTML = generateHTML(filteredJourneys);
+        result.innerHTML = `
+          <table class="ts table striped">
+            <tbody>
+              ${generateHTML(filteredJourneys)}
+            </tbody>
+          </table>
+        `;
         backup = journeys;
       } else {
         result.innerHTML = 'No result.';
@@ -185,8 +169,6 @@ const services = [{label:"All",value:null,belongsTo:"S"},{label:"All",value:null
 
   function generateLink() {
     const {
-      username,
-      password,
       depart,
       arrival,
       is_round,
@@ -249,38 +231,47 @@ pax_price=${paxPriceFilter.value}`.replace(/\r?\n|\r/g, '')
         const [depart, arrival] = JourneySellKey.match(/~([A-Z_]{3})~/g);
         const [start, end] = JourneySellKey.match(/~\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}~/g);
         return `
-          <div class="row">
-            <div>
+          <tr class="row">
+            <td>
               ${normalizeDate(start).date}
-            </div>
-            <div>
+            </td>
+            <td>
               ${normalizeDate(start).time}
-            </div>
-            <div>
+            </td>
+            <td>
               ${normalizeDate(end).date}
-            </div>
-            <div>
+            </td>
+            <td>
               ${normalizeDate(end).time}
-            </div>
-            <div class="desktop-only">
+            </td>
+            <td class="desktop-only">
               ${mapClassById(ProductClass)}
-            </div>
-            <div class="mobile-only">
-              ${ProductClass}
-            </div>
-            <div class="desktop-only">
+            </td>
+            <td class="desktop-only">
               ${ClassOfServiceName}
-            </div>
-            <div class="mobile-only">
-              ${ClassOfService}
-            </div>
-            <div>
+            </td>
+            <td class="desktop-only">
               ${AvailableCount === 0 ? 'N' : AvailableCount} Seats
-            </div>
-            <div>
+            </td>
+            <td class="desktop-only">
               ${FullFarePrice > DiscountedFarePrice ? `<span style='color: red;'>${DiscountedFarePrice}</span>` : FullFarePrice}€
-            </div>
-          </div>`
+            </td>
+          </tr>
+          <tr class="mobile-only">
+            <td>
+              ${mapClassById(ProductClass)}
+            </td>
+            <td>
+              ${ClassOfServiceName}
+            </td>
+            <td>
+              ${AvailableCount === 0 ? 'N' : AvailableCount} Seats
+            </td>
+            <td>
+              ${FullFarePrice > DiscountedFarePrice ? `<span style='color: red;'>${DiscountedFarePrice}</span>` : FullFarePrice}€
+            </td>
+          </tr>
+          `
         ;
       }).join('');
     }).join('');
